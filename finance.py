e2cb6304c1bc7f4dae78d080552d70672b0768fe
SenpaiFlo <florin.curiger@gmx.ch>
2023-11-29 16:09:51 +0100

    Update Finance.py

--- finance.py
+++ finance.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 #Autoren: Florin Curiger, Enrique Munoz und Karma Khamritshang
 #-------------------------------------------------------------
+
+import yfinance as yf
+
+BTC_Ticker = yf.Ticker("BTC-USD")
+BTC_Data = BTC_Ticker.history(period="max")
+
+print(BTC_Data)
